9cad6942c65337b75ef0b1d06f7ac616dbf72c77
maxinespencer9 <maxinespencer9@gmail.com>
2025-02-05 20:19:56 -0500

    work on search function

--- movies/views.py
+++ movies/views.py
@@ -29,3 +29,10 @@ def index(request):
     template_data['movies'] = movies
     return render(request, 'movies/index.html',
                   {'template_data': template_data})
+
+def show(request, id):
+    movie = movies[id - 1]
+    template_data = {}
+    template_data['title'] = movie['name']
+    template_data['movie'] = movie
+    return render(request, 'movies/show.html', {'template_data': template_data})
